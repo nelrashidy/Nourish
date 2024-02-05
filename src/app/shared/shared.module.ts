7df9c1494e73b5540d3f90bc2207993ddd8f554c
nelrashidy <89921231+nelrashidy@users.noreply.github.com>
2024-02-05 22:40:57 +0200

    footerUi+addit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+
+const declarations = [NavbarComponent,
+  FooterComponent,
+  NewsletterComponent]
 
 @NgModule({
   declarations: [
-    NavbarComponent,
-    FooterComponent
+    [...declarations]
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent,
-    FooterComponent
+    [...declarations]
   ]
 })
 export class SharedModule { }
